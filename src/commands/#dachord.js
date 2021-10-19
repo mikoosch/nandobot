22cda5cce5b5774a887014d16efe1cdd413ac691
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-20 00:01:49 +0200

    shauns chord finalized

--- src/commands/#dachord.js
+++ src/commands/#dachord.js
@@ -1,5 +1,5 @@
 const { client } = require("tmi.js")
 
 exports.run = (client, message, args , user , channel , self) => {
-    client.say(channel, "Dominant 7 #9 #11 b5 b13 b9 38.4 with a tritone sub but played in the Phrygian b2 b5 scale and its in drop 2 voicing and the b7 is 38.4 cents flat")
+    client.say(channel, "Dominant 7 #9 #11 b5 b13 b9 with a tritone sub but played in the Phrygian b2 b5 scale and its in drop 2 voicing and the b7 is 38.4 cents flat")
 }
