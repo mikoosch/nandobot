3b2d1d85c8a4e5ba708c47cfafac08ca2e473d32
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-19 22:17:05 +0200

    we gucci now?

--- src/commands/#cap.js
+++ src/commands/#cap.js
@@ -2,14 +2,14 @@ const { client } = require("tmi.js")
 
 exports.run = (client, message, args, user, channel, self) => {
     const cap = [
-        "That's fucking :billed_cap: my guy",
-        "That's fucking :billed_cap: my guy",
-        "That's fucking :billed_cap: my guy",
-        "That's fucking :billed_cap: my guy",
-        "I ain't never seen anybody :billed_cap: this much",
-        "That's straight fax :100:",
-        "That'sstraight fax :100:",
-        "That's straight fax :100:"
+        "Thats fucking :billed_cap: my guy",
+        "Thats fucking :billed_cap: my guy",
+        "Thats fucking :billed_cap: my guy",
+        "Thats fucking :billed_cap: my guy",
+        "I aint never seen anybody :billed_cap: this much",
+        "Thats straight fax :100:",
+        "Thats straight fax :100:",
+        "Thats straight fax :100:"
     ]
 
     const random = Math.floor(Math.random() * phrases.length);
