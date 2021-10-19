3007a005e2868d9dc7dbaa3ac8f8db032f7f54ce
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-19 22:15:51 +0200

    finishing touches pt2

--- src/commands/#cap.js
+++ src/commands/#cap.js
@@ -7,10 +7,9 @@ exports.run = (client, message, args, user, channel, self) => {
         "That's fucking :billed_cap: my guy",
         "That's fucking :billed_cap: my guy",
         "I ain't never seen anybody :billed_cap: this much",
-        "straight fax :100:",
-        "straight fax :100:",
-        "straight fax :100:"
-
+        "That's straight fax :100:",
+        "That'sstraight fax :100:",
+        "That's straight fax :100:"
     ]
 
     const random = Math.floor(Math.random() * phrases.length);
