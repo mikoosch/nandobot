6ccdc6b6a779f6dc897919a83e5f17c0e7fa4e3f
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-19 17:15:29 +0200

    cleaning up some commands

--- src/commands/#cap.js
+++ src/commands/#cap.js
@@ -1,11 +1,19 @@
 const { client } = require("tmi.js")
 
 exports.run = (client, message, args, user, channel, self) => {
-    const cap = [
-        "fucking cap my guy :billed_cap",
-        "straight fax :100:"
-    ]
+    const phrases = [
+        "That's fucking cap :billed_cap: my guy",
+        "That's fucking :billed_cap: my guy",
+        "That's fucking cap :billed_cap: my guy",
+        "That's fucking :billed_cap: my guy",
+        "That's fucking cap :billed_cap: my guy",
+        "That's fucking :billed_cap: my guy",
+        "I ain't never seen anybody :billed_cap: this much",
+        "That's straight fax :100:",
+        "That's straight fax :100:",
+        "That's straight fax :100:"
+        ]
 
     const random = Math.floor(Math.random() * phrases.length);
-    client.say(channel, `That's ${cap[random]}`)
+    client.say(channel, phrases[random])
 }
