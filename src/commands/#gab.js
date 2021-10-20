edac84c04dc866f7813316791c6c2fe36c9d623a
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-20 05:24:44 +0200

    gab command

--- src/commands/#gab.js
+++ src/commands/#gab.js
@@ -1,7 +1,7 @@
 const { client } = require("tmi.js")
 
 exports.run = (client, message, args , user , channel , self) => {
-    client.say(channel, "/me Gab Creates Music Library dropping soon")
+    client.say(channel, "Go check out Gabcreates: https://sptfy.com/gab")
 }
 
 
