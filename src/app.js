6d0ef5e74b69c0fff4e57520a5e5e29f77c0b1b9
Miko <92799034+mikoosch@users.noreply.github.com>
2021-10-22 00:49:35 +0200

    removing mak stream

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const options = {
         username: process.env.BOT_USERNAME,
         password: process.env.OAUTH_TOKEN
     },
-    channels: ["mikoosch", "chefjoi", "99makavelii"]
+    channels: ["mikoosch", "chefjoi"]
 }
 
 const client = new tmi.Client(options)
